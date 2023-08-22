d6be681dfdfba5e11513d91b3dd8dc0014bbf93a
Sudhakarsiva18 <108916787+Sudhakarsiva18@users.noreply.github.com>
2023-08-22 10:50:22 +0530

    Add files via upload

---
+++ Practiceathome/Day3_homework/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
+
+@RestController
+public class StudentController {
+
+	@Autowired
+	StudentService ser;
+	
+	@PostMapping("adddetails")
+	public Student add(@RequestBody Student s) {
+		return ser.getinfo(s);
+	}
+	
+	@GetMapping("showdetails")
+	public List<Student> show(){
+		return ser.showinfo();
+	}
+}
---
+++ Practiceathome/Day3_homework/src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepo;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	StudentRepo rep;
+	
+	public Student getinfo(Student s) {
+		return rep.save(s);
+	}
+	
+	public List<Student> showinfo(){
+		return rep.findAll();
+	}
+}
